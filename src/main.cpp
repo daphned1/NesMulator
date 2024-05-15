b1e6adb1bebf185804e040d1dac6f308e2661e3a
Daphne Dao <daphnedao19@gmail.com>
2024-05-14 19:08:27 -0700

    fixed ppu and controlers

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,87 @@
+/*
+	olc::NES - Part #5 - PPU Rendering - Foregrounds
+	"Thanks Dad for believing computers were gonna be a big deal..." - javidx9
+
+	License (OLC-3)
+	~~~~~~~~~~~~~~~
+
+	Copyright 2018-2019 OneLoneCoder.com
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions
+	are met:
+
+	1. Redistributions or derivations of source code must retain the above
+	copyright notice, this list of conditions and the following disclaimer.
+
+	2. Redistributions or derivative works in binary form must reproduce
+	the above copyright notice. This list of conditions and the following
+	disclaimer must be reproduced in the documentation and/or other
+	materials provided with the distribution.
+
+	3. Neither the name of the copyright holder nor the names of its
+	contributors may be used to endorse or promote products derived
+	from this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+	A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+	LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+	THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+	Relevant Video: https://youtu.be/cksywUTZxlY
+
+	Links
+	~~~~~
+	YouTube:	https://www.youtube.com/javidx9
+				https://www.youtube.com/javidx9extra
+	Discord:	https://discord.gg/WhwHUMV
+	Twitter:	https://www.twitter.com/javidx9
+	Twitch:		https://www.twitch.tv/javidx9
+	GitHub:		https://www.github.com/onelonecoder
+	Patreon:	https://www.patreon.com/javidx9
+	Homepage:	https://www.onelonecoder.com
+
+	Author
+	~~~~~~
+	David Barr, aka javidx9, ©OneLoneCoder 2019
+*/
+
 #include <iostream>
 #include <sstream>
 
 #include "bus.h"
-#include "CPU6502.h"
+#include "ppu2C02.h"
 
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
 
 
-class Demo_olc6502 : public olc::PixelGameEngine
+class Demo_olc2C02 : public olc::PixelGameEngine
 {
 public:
-	Demo_olc6502() { sAppName = "olc6502 Demonstration"; }
+	Demo_olc2C02() { sAppName = "olc2C02 Demonstration"; }
 
-private: 
-	std::shared_ptr<cartridge> cart;
+private:
+	// The NES
 	Bus nes;
+	std::shared_ptr<cartridge> cart;
+	bool bEmulationRun = false;
+	float fResidualTime = 0.0f;
+
+	uint8_t nSelectedPalette = 0x00;
+
+private:
+	// Support Utilities
 	std::map<uint16_t, std::string> mapAsm;
-
-	bool emulationRun = false;
-	float residualT = 0.0f;
-
-	uint8_t selectedPalette = 0x00; // allow user to choose which palette is being used to draw the pattern table
 
 	std::string hex(uint32_t n, uint8_t d)
 	{
@@ -101,22 +160,20 @@ private:
 
 	bool OnUserCreate()
 	{
-		// load cartridge
-		cart = std::make_shared<cartridge>("../games/nestest.nes");
+		// Load the cartridge
+		cart = std::make_shared<cartridge>("../games/Super_mario_brothers.nes");
 
-		if (!cart->ImageValid()) {
+		if (!cart->ImageValid())
 			return false;
-		}
-		
-		//insert into nes
+
+		// Insert into NES
 		nes.insertCart(cart);
 
-		// extract disassembly
+		// Extract dissassembly
 		mapAsm = nes.cpu.disassemble(0x0000, 0xFFFF);
 
-		// reset nes
+		// Reset NES
 		nes.reset();
-
 		return true;
 	}
 
@@ -124,106 +181,86 @@ private:
 	{
 		Clear(olc::DARK_BLUE);
 
-		// controller
+		// Handle input for controller in port #1
 		nes.controller[0] = 0x00;
-		nes.controller[0] |= GetKey(olc::Key::X).bHeld ? 0x80 : 0x00; // A button
-		nes.controller[0] |= GetKey(olc::Key::Z).bHeld ? 0x40 : 0x00; // B button
-		nes.controller[0] |= GetKey(olc::Key::A).bHeld ? 0x20 : 0x00; // Select
-		nes.controller[0] |= GetKey(olc::Key::S).bHeld ? 0x10 : 0x00; // Start
+		nes.controller[0] |= GetKey(olc::Key::X).bHeld ? 0x80 : 0x00;     // A Button
+		nes.controller[0] |= GetKey(olc::Key::Z).bHeld ? 0x40 : 0x00;     // B Button
+		nes.controller[0] |= GetKey(olc::Key::A).bHeld ? 0x20 : 0x00;     // Select
+		nes.controller[0] |= GetKey(olc::Key::S).bHeld ? 0x10 : 0x00;     // Start
 		nes.controller[0] |= GetKey(olc::Key::UP).bHeld ? 0x08 : 0x00;
 		nes.controller[0] |= GetKey(olc::Key::DOWN).bHeld ? 0x04 : 0x00;
 		nes.controller[0] |= GetKey(olc::Key::LEFT).bHeld ? 0x02 : 0x00;
 		nes.controller[0] |= GetKey(olc::Key::RIGHT).bHeld ? 0x01 : 0x00;
 
-		if (GetKey(olc::Key::R).bPressed) {
-			nes.reset();
-		}
+		if (GetKey(olc::Key::SPACE).bPressed) bEmulationRun = !bEmulationRun;
+		if (GetKey(olc::Key::R).bPressed) nes.reset();
+		if (GetKey(olc::Key::P).bPressed) (++nSelectedPalette) &= 0x07;
 
-		if (GetKey(olc::Key::SPACE).bPressed) {
-			emulationRun = !emulationRun;
-		}
-
-		if (GetKey(olc::Key::P).bPressed) {
-			// increments and wrap it around
-			(++selectedPalette) &= 0x07;
-		}
-
-		if (emulationRun) {
-			if (residualT > 0.0f) {
-				residualT -= fElapsedTime;
-			}
-			else {
-				residualT += (1.0f / 60.0f) - fElapsedTime;
-				do {
-					nes.clock();
-				} while (!nes.ppu.frameComplete);
-				nes.ppu.frameComplete = false;
+		if (bEmulationRun)
+		{
+			if (fResidualTime > 0.0f)
+				fResidualTime -= fElapsedTime;
+			else
+			{
+				fResidualTime += (1.0f / 60.0f) - fElapsedTime;
+				do { nes.clock(); } while (!nes.ppu.frame_complete);
+				nes.ppu.frame_complete = false;
 			}
 		}
-		else {
-
-			// emulate code step by step
-			if (GetKey(olc::Key::C).bPressed) {
-				// clock enough times to execute a whole CPU instruction
-				do {
-					nes.clock();
-				} while (!nes.cpu.complete());
-
-				// cpu clock runs slower than system clock -> may have completed for additional system clock cycles
-				// drain it out
-				do {
-					nes.clock();
-				} while (nes.cpu.complete());
+		else
+		{
+			// Emulate code step-by-step
+			if (GetKey(olc::Key::C).bPressed)
+			{
+				// Clock enough times to execute a whole CPU instruction
+				do { nes.clock(); } while (!nes.cpu.complete());
+				// CPU clock runs slower than system clock, so it may be
+				// complete for additional system clock cycles. Drain
+				// those out
+				do { nes.clock(); } while (nes.cpu.complete());
 			}
 
-			// emulate the whole frame
-			if (GetKey(olc::Key::F).bPressed) {
-				// clock enough times to draw a frame
-				do {
-					nes.clock();
-				} while (!nes.ppu.frameComplete);
-
-				// use residual clock cycles to complete current instruction
-				do {
-					nes.clock();
-				} while (!nes.cpu.complete());
-				// reset frame completion flag
-				nes.ppu.frameComplete = false;
+			// Emulate one whole frame
+			if (GetKey(olc::Key::F).bPressed)
+			{
+				// Clock enough times to draw a single frame
+				do { nes.clock(); } while (!nes.ppu.frame_complete);
+				// Use residual clock cycles to complete current instruction
+				do { nes.clock(); } while (!nes.cpu.complete());
+				// Reset frame completion flag
+				nes.ppu.frame_complete = false;
 			}
 		}
 
 		DrawCpu(516, 2);
 		//DrawCode(516, 72, 26);
 
-		for (int i = 0; i < 26; i++) {
-			std::string s = hex(i, 2) + ": (" + std::to_string(nes.ppu.OAM_ptr[i * 4 + 3])
-				+ ", " + std::to_string(nes.ppu.OAM_ptr[i * 4 + 0]) + ") "
-				+ "ID: " + hex(nes.ppu.OAM_ptr[i * 4 + 1], 2) +
-				+" AT: " + hex(nes.ppu.OAM_ptr[i * 4 + 2], 2);
+		// Draw OAM Contents (first 26 out of 64) ======================================
+		for (int i = 0; i < 26; i++)
+		{
+			std::string s = hex(i, 2) + ": (" + std::to_string(nes.ppu.pOAM[i * 4 + 3])
+				+ ", " + std::to_string(nes.ppu.pOAM[i * 4 + 0]) + ") "
+				+ "ID: " + hex(nes.ppu.pOAM[i * 4 + 1], 2) +
+				+" AT: " + hex(nes.ppu.pOAM[i * 4 + 2], 2);
 			DrawString(516, 72 + i * 10, s);
 		}
 
-		const int swatchSize = 6;
-		for (int p = 0; p < 8; p++) { // for each palette
-			for (int s = 0; s < 4; s++) { // for each index
-				FillRect(516 + p * (swatchSize * 5) + s * swatchSize, 340, swatchSize, swatchSize, nes.ppu.getColorFromPalette(p, s));
-			}
-		}
+		// Draw Palettes & Pattern Tables ==============================================
+		const int nSwatchSize = 6;
+		for (int p = 0; p < 8; p++) // For each palette
+			for (int s = 0; s < 4; s++) // For each index
+				FillRect(516 + p * (nSwatchSize * 5) + s * nSwatchSize, 340,
+					nSwatchSize, nSwatchSize, nes.ppu.GetColorFromPaletteRam(p, s));
 
-		DrawRect(516 + selectedPalette * (swatchSize * 5) - 1, 339, (swatchSize * 4), swatchSize, olc::WHITE);
+		// Draw selection reticule around selected palette
+		DrawRect(516 + nSelectedPalette * (nSwatchSize * 5) - 1, 339, (nSwatchSize * 4), nSwatchSize, olc::WHITE);
 
-		DrawSprite(516, 348, &nes.ppu.GetPatternTable(0, selectedPalette));
-		DrawSprite(648, 348, &nes.ppu.GetPatternTable(1, selectedPalette));
+		// Generate Pattern Tables
+		DrawSprite(516, 348, &nes.ppu.GetPatternTable(0, nSelectedPalette));
+		DrawSprite(648, 348, &nes.ppu.GetPatternTable(1, nSelectedPalette));
 
+		// Draw rendered output ========================================================
 		DrawSprite(0, 0, &nes.ppu.GetScreen(), 2);
-
-		// draw ids (for testing)
-		//for (uint8_t y = 0; y < 30; y++) {
-		//	for (uint8_t x = 0; x < 32; x++) {
-		//		DrawString(x * 16, y * 16, hex((uint32_t)nes.ppu.nameTable[0][y * 32 + x], 2));
-		//	}
-		//}
-
 		return true;
 	}
 };
@@ -234,7 +271,7 @@ private:
 
 int main()
 {
-	Demo_olc6502 demo;
+	Demo_olc2C02 demo;
 	demo.Construct(780, 480, 2, 2);
 	demo.Start();
 	return 0;
